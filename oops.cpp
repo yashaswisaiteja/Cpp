44255353fd18fce614005046918130a0d0247d72
yashaswisaiteja <119913198+yashaswisaiteja@users.noreply.github.com>
2023-08-29 11:05:47 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+//class
+class Human{
+public:
+	string name;
+	void Display()
+	{
+		//cout<<name<<endl;
+		cout<<"hi i am "<<name<<endl;
+	}
+};
+int main()
+{
+	//various ways to objection creation
+	Human yashu;
+	yashu.name="yashu";
+	yashu.Display();//calling the dot()
+
+	//Human *gayathri = new Human();//creating using pointer
+	//gayathri->name="gayathri";//creating using an arrow()
+	Human *gayathri = &yashu;  //class pointer
+	gayathri->Display();     //(*gyathri).name
+    return 0;
+}
